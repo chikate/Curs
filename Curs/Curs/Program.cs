b948aafc3f1e1fc62ab9e9174061e2e510591bb3
chikate <tadragos@yahoo.com>
2023-09-26 21:04:30 +0300

    input implemented

--- Curs/Curs/Program.cs
+++ Curs/Curs/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = Console.ReadLine();
+            Console.WriteLine("Hello," + input + "!");
         }
     }
 }
