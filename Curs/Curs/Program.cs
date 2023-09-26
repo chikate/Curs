73d4da4bff334d8b8f5f2d546e595a8bd83173a5
chikate <tadragos@yahoo.com>
2023-09-26 21:36:40 +0300

    Curs 1 final code

--- Curs/Curs/Program.cs
+++ Curs/Curs/Program.cs
@@ -2,11 +2,25 @@
 {
     internal class Program
     {
+        static void tryGetAge(string name)
+        {
+            Console.Write("Enter your age: ");
+            string ageString = Console.ReadLine();
+            if (int.TryParse(ageString, out int age))
+            {
+                Console.WriteLine("Hello, " + name + " your age is: " + age + "! :)");
+            }
+            else
+            {
+                Console.WriteLine("Invalid age!");
+                tryGetAge(name);
+            }
+        }
         static void Main(string[] args)
         {
             Console.Write("Enter your name: ");
-            string input = Console.ReadLine();
-            Console.WriteLine("Hello, " + input + "! :)");
+            string name = Console.ReadLine();
+            tryGetAge(name);
         }
     }
 }
